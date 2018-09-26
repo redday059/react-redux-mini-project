6e32123af30c612ed53c8afad1be8acda2f7f85d
Nataliya Druzina <redday@Nataliyas-MacBook-Pro.local>
2018-09-26 12:28:08 +0200

    restores ServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.querySelector('#root'),
 );
+registerServiceWorker();
